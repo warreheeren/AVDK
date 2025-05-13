7efd46256feadd2dc8f6347140fb996a5c2beab6
Warre <warre.heeren@hotmail.com>
2025-05-13 14:44:35 +0200

    csrf fix?

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,7 @@
 <html>
 
 <head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <title>Laravel Inertia App</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
